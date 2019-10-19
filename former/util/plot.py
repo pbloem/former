9ae4517d69d78615d9548b54d2af281c966a1c59
Peter Bloem <git@peterbloem.nl>
2019-10-19 19:32:51 +0200

    Add exp branch, plot cosine similarities

---
+++ former/util/plot.py
@@ -0,0 +1,33 @@
+import torch
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+def cosine_heatmap(vectors, ax=plt.gca(), cm='inferno'):
+    """
+    Plot a heatmap of the cosine similarities between the given vectors
+    :param vectors:
+    :param axes: Default is current axes
+    :return:
+    """
+
+    num, dim = vectors.size()
+    print(num, dim)
+
+    norms = vectors.norm(dim=1, keepdim=True)
+
+    dots = torch.mm(vectors, vectors.t())
+    div  = torch.mm(norms, norms.t())
+
+    im = (dots/div).cpu().numpy()
+
+    print(im.min(), im.max())
+    print(im.shape)
+
+    print(im[10:, 10:])
+
+    ax.imshow(im)
+
+    return
+
