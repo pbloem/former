aeb907af12539fab6ef63edd630b9225bb985d36
Bastiaan Quast <bquast@gmail.com>
2021-05-08 15:50:21 +0200

    import SelfAttentionWide (2x) -> import SelfAttentionNarrow, SelfAttentionWide

--- former/__init__.py
+++ former/__init__.py
@@ -1,4 +1,4 @@
-from .modules import SelfAttentionWide, SelfAttentionWide, TransformerBlock
+from .modules import SelfAttentionNarrow, SelfAttentionWide, TransformerBlock
 
 from .transformers import GTransformer, CTransformer
 
